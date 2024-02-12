8d3dd4f7e6442dd94951a7b53c33e1dd62df7deb
Adam <adam@gatherer.tech>
2024-02-12 15:42:02 +0000

    Annoted with comments

--- infra/apig.tf
+++ infra/apig.tf
@@ -1,9 +1,10 @@
+# Creating the gateway for the REST API
 resource "aws_api_gateway_rest_api" "tf-swapi" {
     # Name & description
     name = "TF-SWAPI"
     description = "API Gateway to proxy requests to SWAPI"
     /*
-    # Body specification for OpenAPI
+    # Body specification for OpenAPI - we're not doing this!
     body = jsonencode({
         openapi = "3.0.1"
         info = {
@@ -30,29 +31,43 @@ resource "aws_api_gateway_rest_api" "tf-swapi" {
     }
 }
 
+# Creating the resource, this is the endpoint to which users send requests
 resource "aws_api_gateway_resource" "tf-swapi-resource" {
     parent_id = aws_api_gateway_rest_api.tf-swapi.root_resource_id
     rest_api_id = aws_api_gateway_rest_api.tf-swapi.id
     path_part = "people"
 }
 
+# the method users can use against our API
 resource "aws_api_gateway_method" "tf-swapi-method" {
     authorization = "NONE"
     http_method = "GET"
+    # ID of the resource
     resource_id = aws_api_gateway_resource.tf-swapi-resource.id
+    # ID of our REST API
     rest_api_id = aws_api_gateway_rest_api.tf-swapi.id
 }
 
+# The gateway integration is where the API requests are sent
 resource "aws_api_gateway_integration" "tf-swapi-integration" {
+    # ID of our REST API
     rest_api_id = aws_api_gateway_rest_api.tf-swapi.id
+    # ID of the resource
     resource_id = aws_api_gateway_resource.tf-swapi-resource.id
+    # The HTTP method used 
     http_method = aws_api_gateway_method.tf-swapi-method.http_method
+    # the type of integration we're using
     type = "HTTP"
+    # URI to which the requests are sent
     uri = "https://swapi.dev/api/people"
+    # the type of method used when sending the request
     integration_http_method = "GET"
+    # makes sure tf won't start deploying this resource until the
+    # specified resource has already been deployed
     depends_on = [ aws_api_gateway_method.tf-swapi-method ]
 }
 
+# we'll get to this later
 /*
 resource "aws_api_gateway_stage" "tf-swapi-stage" {
     deployment_id = aws_api_gateway_rest_api.tf-swapi.id
