c599c2c8ddbfdc4c34f17919ce07d1a813dda50a
Adam G <adam@gatherer.tech>
2024-02-15 18:57:02 +0000

    Fixed formatting

--- infra/apig.tf
+++ infra/apig.tf
@@ -1,9 +1,9 @@
 # Creating the gateway for the REST API
 resource "aws_api_gateway_rest_api" "tf-swapi" {
-    # Name & description
-    name = "TF-SWAPI"
-    description = "API Gateway to proxy requests to SWAPI"
-    /*
+  # Name & description
+  name        = "TF-SWAPI"
+  description = "API Gateway to proxy requests to SWAPI"
+  /*
     # Body specification for OpenAPI - we're not doing this!
     body = jsonencode({
         openapi = "3.0.1"
@@ -23,60 +23,60 @@ resource "aws_api_gateway_rest_api" "tf-swapi" {
         }
     })
     */
-    # Configuring the endpoint
-    endpoint_configuration {
-      # Setting the API to route requests to the nearest
-      # CloudFront Point of Presence
-      types = ["EDGE"]
-    }
+  # Configuring the endpoint
+  endpoint_configuration {
+    # Setting the API to route requests to the nearest
+    # CloudFront Point of Presence
+    types = ["EDGE"]
+  }
 }
 /*********************************
 Creating the /people API endpoint
 *********************************/
 # Creating the resource, this is the endpoint to which users send requests
 resource "aws_api_gateway_resource" "tf-swapi-people-resource" {
-    parent_id = aws_api_gateway_rest_api.tf-swapi.root_resource_id
-    rest_api_id = aws_api_gateway_rest_api.tf-swapi.id
-    path_part = "people"
+  parent_id   = aws_api_gateway_rest_api.tf-swapi.root_resource_id
+  rest_api_id = aws_api_gateway_rest_api.tf-swapi.id
+  path_part   = "people"
 }
 
 # the method users can use against our API
 resource "aws_api_gateway_method" "tf-swapi-people-method" {
-    authorization = "NONE"
-    http_method = "GET"
-    # ID of the resource using this method
-    resource_id = aws_api_gateway_resource.tf-swapi-people-resource.id
-    # ID of our REST API
-    rest_api_id = aws_api_gateway_rest_api.tf-swapi.id
-    # setting up the query strings for the method request
-    request_parameters = {
-        "method.request.querystring.search" = true,
-        "method.request.querystring.format" = true
-    }
+  authorization = "NONE"
+  http_method   = "GET"
+  # ID of the resource using this method
+  resource_id = aws_api_gateway_resource.tf-swapi-people-resource.id
+  # ID of our REST API
+  rest_api_id = aws_api_gateway_rest_api.tf-swapi.id
+  # setting up the query strings for the method request
+  request_parameters = {
+    "method.request.querystring.search" = true,
+    "method.request.querystring.format" = true
+  }
 }
 
 # The gateway integration is where the API requests are sent
 resource "aws_api_gateway_integration" "tf-swapi-people-method-integration" {
-    # ID of our REST API
-    rest_api_id = aws_api_gateway_rest_api.tf-swapi.id
-    # ID of the resource
-    resource_id = aws_api_gateway_resource.tf-swapi-people-resource.id
-    # The HTTP method used 
-    http_method = aws_api_gateway_method.tf-swapi-people-method.http_method
-    # the type of integration we're using
-    type = "HTTP"
-    # URI to which the requests are sent
-    uri = "https://swapi.dev/api/people"
-    # the type of method used when sending the request
-    integration_http_method = "GET"
-    # configuring the integration request query strings
-    request_parameters = {
-        "integration.request.querystring.search" = "method.request.querystring.search",
-        "integration.request.querystring.format" = "method.request.querystring.format"
-    }
-    # makes sure tf won't start deploying this resource until the
-    # specified resource has already been deployed
-    depends_on = [ aws_api_gateway_method.tf-swapi-people-method ]
+  # ID of our REST API
+  rest_api_id = aws_api_gateway_rest_api.tf-swapi.id
+  # ID of the resource
+  resource_id = aws_api_gateway_resource.tf-swapi-people-resource.id
+  # The HTTP method used 
+  http_method = aws_api_gateway_method.tf-swapi-people-method.http_method
+  # the type of integration we're using
+  type = "HTTP"
+  # URI to which the requests are sent
+  uri = "https://swapi.dev/api/people"
+  # the type of method used when sending the request
+  integration_http_method = "GET"
+  # configuring the integration request query strings
+  request_parameters = {
+    "integration.request.querystring.search" = "method.request.querystring.search",
+    "integration.request.querystring.format" = "method.request.querystring.format"
+  }
+  # makes sure tf won't start deploying this resource until the
+  # specified resource has already been deployed
+  depends_on = [aws_api_gateway_method.tf-swapi-people-method]
 }
 
 /*********************************
@@ -84,24 +84,24 @@ Creating the /people/{id} endpoint
 *********************************/
 # Creating the "sub resource" /people/{id}
 resource "aws_api_gateway_resource" "tf-swapi-people-id-resource" {
-    # sub resource, so the parent id is that of "/people/"
-    parent_id = aws_api_gateway_resource.tf-swapi-people-resource.id
-    rest_api_id = aws_api_gateway_rest_api.tf-swapi.id
-    # "{id}" is a variable so we can request any id we'd like
-    path_part = "{id}"
+  # sub resource, so the parent id is that of "/people/"
+  parent_id   = aws_api_gateway_resource.tf-swapi-people-resource.id
+  rest_api_id = aws_api_gateway_rest_api.tf-swapi.id
+  # "{id}" is a variable so we can request any id we'd like
+  path_part = "{id}"
 }
 
 resource "aws_api_gateway_method" "tf-swapi-people-id-method" {
-    authorization = "NONE"
-    http_method = "GET"
-    # ID of the resource using this method
-    resource_id = aws_api_gateway_resource.tf-swapi-people-id-resource.id
-    # ID of our REST API
-    rest_api_id = aws_api_gateway_rest_api.tf-swapi.id
-    # setting up the query strings for the method request
-    request_parameters = {
-        "method.request.querystring.format" = true
-    }
+  authorization = "NONE"
+  http_method   = "GET"
+  # ID of the resource using this method
+  resource_id = aws_api_gateway_resource.tf-swapi-people-id-resource.id
+  # ID of our REST API
+  rest_api_id = aws_api_gateway_rest_api.tf-swapi.id
+  # setting up the query strings for the method request
+  request_parameters = {
+    "method.request.querystring.format" = true
+  }
 }
 
 
--- infra/main.tf
+++ infra/main.tf
@@ -1,20 +1,20 @@
 terraform {
-    required_providers {
-      aws = {
-        version = "~> 5.36.0"
-        source = "hashicorp/aws"
-      }
+  required_providers {
+    aws = {
+      version = "~> 5.36.0"
+      source  = "hashicorp/aws"
     }
+  }
 }
 
 provider "aws" {
-    region = "eu-west-1"
+  region = "eu-west-1"
 }
 
 terraform {
-    backend "s3" {
-        bucket = "terrified-banana-tf-states"
-        region = "eu-west-1"
-        key = "tf-aws-swapi/state/main"
-    }
+  backend "s3" {
+    bucket = "terrified-banana-tf-states"
+    region = "eu-west-1"
+    key    = "tf-aws-swapi/state/main"
+  }
 }
